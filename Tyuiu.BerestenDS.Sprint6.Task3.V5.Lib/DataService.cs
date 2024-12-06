ee0535dfb8b81653dfaf941e9d2cd5f0ecfbd3a8
Денис Берестень <d.blyl1337@gmail.com>
2024-12-06 19:01:35 +0500

    Task3

--- Tyuiu.BerestenDS.Sprint6.Task3.V5.Lib/DataService.cs
+++ Tyuiu.BerestenDS.Sprint6.Task3.V5.Lib/DataService.cs
@@ -8,33 +8,29 @@ namespace Tyuiu.BerestenDS.Sprint6.Task3.V5.Lib
             int rows = matrix.GetLength(0);
             int cols = matrix.GetLength(1);
 
-            // Проверяем, что массив имеет хотя бы 3 столбца
+            // Проверяем, что матрица имеет хотя бы 3 столбца
             if (cols < 3)
             {
-                throw new ArgumentException("Массив должен содержать как минимум 3 столбца.");
+                throw new ArgumentException("Матрица должна содержать как минимум 3 столбца.");
             }
 
-            // Создаем временный массив для хранения строк
-            var tempArray = new (int, int, int, int, int)[rows];
+            // Преобразуем матрицу в список кортежей
+            var tempList = Enumerable.Range(0, rows)
+                .Select(i => (matrix[i, 0], matrix[i, 1], matrix[i, 2], matrix[i, 3], matrix[i, 4]))
+                .ToList();
 
-            // Заполняем временный массив
-            for (int i = 0; i < rows; i++)
-            {
-                tempArray[i] = (matrix[i, 0], matrix[i, 1], matrix[i, 2], matrix[i, 3], matrix[i, 4]);
-            }
-
-            // Сортируем временный массив по третьему элементу
-            Array.Sort(tempArray, (a, b) => a.Item3.CompareTo(b.Item3));
+            // Сортируем список по третьему элементу
+            var sortedList = tempList.OrderBy(row => row.Item3).ToList();
 
             // Создаем новый отсортированный массив
             int[,] sortedMatrix = new int[rows, cols];
             for (int i = 0; i < rows; i++)
             {
-                sortedMatrix[i, 0] = tempArray[i].Item1;
-                sortedMatrix[i, 1] = tempArray[i].Item2;
-                sortedMatrix[i, 2] = tempArray[i].Item3;
-                sortedMatrix[i, 3] = tempArray[i].Item4;
-                sortedMatrix[i, 4] = tempArray[i].Item5;
+                sortedMatrix[i, 0] = sortedList[i].Item1;
+                sortedMatrix[i, 1] = sortedList[i].Item2;
+                sortedMatrix[i, 2] = sortedList[i].Item3;
+                sortedMatrix[i, 3] = sortedList[i].Item4;
+                sortedMatrix[i, 4] = sortedList[i].Item5;
             }
 
             return sortedMatrix;
