0fd94ee5c7992daf55516429f6d92353adc2ec7e
Денис Берестень <d.blyl1337@gmail.com>
2024-12-08 21:11:18 +0500

    Task6

--- Tyuiu.BerestenDS.Sprint6.Task6.V9.Lib/DataService.cs
+++ Tyuiu.BerestenDS.Sprint6.Task6.V9.Lib/DataService.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.BerestenDS.Sprint6.Task6.V9.Lib
                 string lines;
                 while ((lines = sr.ReadLine()) != null)
                 {
-                    resStr = resStr + lines.Split(' ').LastOrDefault();
+                    resStr = resStr + lines.Split(' ').LastOrDefault()+ " ";
                 }
 
             }
