fd5158b7b6feccfc5818ae0ae7fe458bb576a4e0
Денис Берестень <d.blyl1337@gmail.com>
2024-12-08 19:38:37 +0500

    Task5

---
+++ Tyuiu.BerestenDS.Sprint6.Task5.V16.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.BerestenDS.Sprint6.Task5.V16.Lib;
+namespace Tyuiu.BerestenDS.Sprint6.Task5.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.BerestenDS.Sprint6.Task5.V16/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.BerestenDS.Sprint6.Task5.V16
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
